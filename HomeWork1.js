9a5290950f2be5165c072fad816a56d8b38dd0b4
tatruz <mukuchyan.tatevik@gmail.com>
2021-05-19 11:19:07 +0400

    Add files via upload

---
+++ HomeWork1.js
@@ -0,0 +1,83 @@
+//task 1
+
+let firstName = prompt("Your name is")
+let lastName = prompt("Your surname is")
+alert ("Your name is " + firstName + " and surname is " + lastName )
+
+//task 2
+let a = 8
+let b = 5
+let result = a + b
+alert(result)
+
+//task 3
+let a = 8
+let b = 5
+let result = a - b
+alert(result)
+
+
+let a = 8
+let b = 5
+let result = a - b
+alert(result)
+
+let a = 8
+let b = 5
+let result = a * b
+alert(result)
+
+
+let a = 8
+let b = 5
+let result = a / b
+alert(result)
+
+let a = 8
+let b = 5
+let result = a % b
+alert(result)
+
+//task 3
+var sampleNumber = 78756 
+lastDigit = sampleNumber % 10
+console.log(lastDigit)
+
+var sampleNumber = -122 
+lastDigit = sampleNumber % 10
+console.log(lastDigit)
+
+var sampleNumber = 8 
+lastDigit = sampleNumber % 10
+console.log(lastDigit)
+
+var sampleNumber = 0
+lastDigit = sampleNumber % 10
+console.log(lastDigit)
+
+
+let myName = "Tatev"
+let age = 32
+alert ("My name is " + myName +"," + " I am " + age)
+
+
+
+let a = 8;
+let b = 12;
+
+a = a * b 
+b = a / b
+a = a / b
+
+console.log (a, b)
+
+
+
+let a = 10;
+let b = 20;
+
+a = a * b 
+b = a / b
+a = a / b
+
+console.log (a, b)
